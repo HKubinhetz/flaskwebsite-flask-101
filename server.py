1ebc14c64147ac8a6ad1680836a80475bfd8923f
Henrique Kubinhetz <rickubinhetz@gmail.com>
2022-03-08 21:07:58 -0300

    First Commit on Day 56!

---
+++ server.py
@@ -0,0 +1,15 @@
+# My Personal Website using Flask!
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
